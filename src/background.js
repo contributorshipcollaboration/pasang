863274ad99b0ce8d50fc4770e583879b9881a704
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2023-01-17 10:45:57 -0800

    tried adding gsheets api still not working because of require

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 
-import { nhb_recipe } from './nhb_recipe.js';
+/* import { nhb_recipe } from './nhb_recipe.js';
 
 // autofill function
 function autofill(contributors_info) {
@@ -7,13 +7,22 @@ function autofill(contributors_info) {
     // This function will run in the context of the background.js
     // A separate function is used to better differentiate between multiple, different submission portals later
     nhb_recipe(contributors_info)
-}
+} */
 
-chrome.runtime.onConnect.addListener(function(port) {
-    port.onMessage.addListener(function(msg) {
-        // Fill out submission portal automatically
-        autofill(msg.contributors_info)
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(function (msg) {
+        if (msg.input_type === "schema") {
+            console.log("schema")
+            // Fill out submission portal automatically
+            autofill(msg.contributors_info)
+        } else if (msg.input_type === "url") {
+            console.log("url")
+            chrome.identity.getAuthToken({ interactive: true }, function (token) {
+                console.log(token);
+            });
+        }
+
         // Send message to close popup window
-        port.postMessage({closePopup: true})
+        port.postMessage({ closePopup: true })
     });
 })
--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,12 @@
 import './popup.css';
-// import Ajv from 'ajv'
+//import Ajv from 'ajv'
+// Require does not work
+// const {gapi} = require('googleapis');
+
 
 // Get submit button
 const submit = document.getElementById("submit");
+const submitUrl = document.getElementById("url-submit")
 
 // All tasks will be done in the background.js file
 // popup.js is only used to communicate with the user directly
@@ -12,9 +16,9 @@ var port = chrome.runtime.connect({
 });
 
 // Submit contributors_info input to background.js as JSON
-function sendInput() {
+function sendSchemaInput() {
     // Get contributors info
-    const contributors_info = JSON.parse(document.getElementById("contributor_information").value)
+    const contributors_info = JSON.parse(document.getElementById("contributor_schema").value)
     // Validate input
     // Not working because of CPS policy
 /*     fetch('https://raw.githubusercontent.com/tenzing-contrib/belayer/main/belayer-flat-schema.json')
@@ -23,15 +27,37 @@ function sendInput() {
             const ajv = new Ajv()
             const validate = ajv.compile(schema)
             const valid = validate(data)
-            console.log(valid) 
+            console.log(valid)
         })
         .catch(err => console.error(err)); */
     // Send contributors info to background
-    port.postMessage({contributors_info: contributors_info});
+    port.postMessage({ input_type: "schema", contributors_info: contributors_info });
+}
+
+function sendUrlInput() {
+    const contributor_url = document.getElementById("contributor_url").value;
+    // Find the spreadsheet ID 
+    // TODO: Later put this in background.js
+/*     let parts = contributor_url.split("/");
+    let spreadsheetId = parts[parts.length - 2];
+    console.log(spreadsheetId)
+
+    gapi.client.sheets.spreadsheets.get({
+        spreadsheetId: spreadsheetId,
+        ranges: []
+    })
+    .then(function(response) {
+      console.log(response.result);
+    }, function(reason) {
+      console.error('error: ' + reason.result.error.message);
+    }); */
+
+    port.postMessage({ input_type: "url" })
 }
 
 // Send popup.html input to background.js for autofilling
-submit.addEventListener('click', sendInput);
+submit.addEventListener('click', sendSchemaInput);
+submitUrl.addEventListener('click', sendUrlInput)
 
 // Close popup window if background.js is finished
 port.onMessage.addListener(function(msg) {
